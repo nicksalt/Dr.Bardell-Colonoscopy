d41f085dbeef25f1b2b4008ac170d39eab332d7e
nicksalt <nicksalt24@gmail.com>
2016-06-26 00:02:44 -0400

    Notifications Working

---
+++ app/src/main/java/ca/mobileappsolutions/drbardellcolonoscopy/NotificationReceiver.java
@@ -0,0 +1,49 @@
+package ca.mobileappsolutions.drbardellcolonoscopy;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+/**
+ * Created by Nick on 2016-06-25.
+ */
+
+public class NotificationReceiver extends BroadcastReceiver {
+    String notText;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("TESTING", "RUNNING NOT");
+        genNot(context);
+
+    }
+    public void genNot(Context context){
+        Log.d("TESTING", "RUNNING NOT");
+
+        notText ="Remember, your colonoscopy is in under one week.";
+
+        android.support.v4.app.NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(context.getResources().getString(R.string.app_name))
+                        .setContentText(notText)
+                        .setPriority(Notification.PRIORITY_MAX)
+                        .setAutoCancel(true);
+
+        Intent resultIntent = new Intent(context, HomeScreen.class);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 7,
+                resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotifyMgr =
+                (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        mNotifyMgr.notify(0, mBuilder.build());
+    }
+}
--- app/src/main/java/ca/mobileappsolutions/drbardellcolonoscopy/TimeService.java
+++ app/src/main/java/ca/mobileappsolutions/drbardellcolonoscopy/TimeService.java
@@ -1,11 +1,15 @@
 package ca.mobileappsolutions.drbardellcolonoscopy;
 
 
+import android.annotation.TargetApi;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.IBinder;
-import android.util.Log;
 
 import java.util.Calendar;
 
@@ -16,23 +20,62 @@ import java.util.Calendar;
 public class TimeService extends Service {
 
     private SharedPreferences myPrefs;
+    private SharedPreferences.Editor e;
     private Calendar appointment;
-    private Calendar oneWeek;
-    private Calendar fiveDays;
-    private Calendar threeDays;
+    private long apTime;
     private int month;
     private int day;
     private int year;
     private int hour;
     private int minute;
-    private long apTime;
+    private Intent i1;
+    private Intent i2;
+    private Intent i3;
+    private Intent i4;
+    private Intent i5;
+    private Intent i6;
+    private PendingIntent pi1;
+    private PendingIntent pi2;
+    private PendingIntent pi3;
+    private PendingIntent pi4;
+    private PendingIntent pi5;
+    private PendingIntent pi6;
+    private Calendar not4;
+    private Calendar not5;
+    private Calendar not6;
+    private AlarmManager am1;
+    private AlarmManager am2;
+    private AlarmManager am3;
+    private AlarmManager am4;
+    private AlarmManager am5;
+    private AlarmManager am6;
+
+
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         myPrefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
+        e = myPrefs.edit();
+        appointment = Calendar.getInstance();
         getAppointment();
         appointment.set(year,month, day, hour, minute);
         apTime = appointment.getTimeInMillis();
-        sentNot();
+        e.putInt("not", 0);
+        setIntents();
+        setCalendars();
+        long c = System.currentTimeMillis();
+        am1 =( AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        am2 =( AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+        am3 =( AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+        am4 =( AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+        am5=( AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+        am6 =( AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+        am6.set(AlarmManager.RTC_WAKEUP, not6.getTimeInMillis(), pi6);
+        am5.set(AlarmManager.RTC_WAKEUP, not5.getTimeInMillis(), pi5);
+        am4.set(AlarmManager.RTC_WAKEUP, not4.getTimeInMillis(), pi4);
+        am3.set(AlarmManager.RTC_WAKEUP, apTime-1000*60*60*24*2, pi3);
+        am2.set(AlarmManager.RTC_WAKEUP, apTime-(1000*60*60*24*5), pi2);
+        am1.set(AlarmManager.RTC_WAKEUP,apTime-(1000*60*60*24*7), pi1);
         return Service.START_STICKY;
     }
 
@@ -53,9 +96,29 @@ public class TimeService extends Service {
         day = myPrefs.getInt("day", 1);
         hour = myPrefs.getInt("hour", 12);
         minute = myPrefs.getInt("minute", 0);
-        Log.d("TESTING", Integer.toString(year) + ' ' + Integer.toString(month) + ' ' + Integer.toString(day) + ' ' +
-                Integer.toString(hour) + ' ' + Integer.toString(minute));
+    }
+    public void setIntents(){
+        i1 = new Intent(TimeService.this, NotificationReceiver.class);
+        i2 = new Intent(TimeService.this, NotificationReceiver1.class);
+        i3 = new Intent(TimeService.this, NotificationReciever2.class);
+        i4 = new Intent(TimeService.this, NotificationReciever3.class);
+        i5 = new Intent(TimeService.this, NotificationReciever4.class);
+        i6 = new Intent(TimeService.this, NotificationReciever5.class);
+        pi1 = PendingIntent.getBroadcast(TimeService.this, 0, i1, 0);
+        pi2 = PendingIntent.getBroadcast(TimeService.this, 1, i2, 0);
+        pi3 = PendingIntent.getBroadcast(TimeService.this, 2, i3, 0);
+        pi4 = PendingIntent.getBroadcast(TimeService.this, 3, i4, 0);
+        pi5 = PendingIntent.getBroadcast(TimeService.this, 4, i5, 0);
+        pi6 = PendingIntent.getBroadcast(TimeService.this, 5, i6, 0);
+    }
 
+    public void setCalendars(){
+        not4= Calendar.getInstance();
+        not5= Calendar.getInstance();
+        not6= Calendar.getInstance();
+        not4.set(year, month, day-1, 12, 0);
+        not5.set(year, month, day-1, 12, 0);
+        not6.set(year, month, day-1, 12, 0);
     }
 
 
